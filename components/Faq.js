474bd2f7e5c291fe0fdb6c0d237eb5da8aa9ceca
singhAkash790 <113873172+singhAkash790@users.noreply.github.com>
2024-01-08 17:01:19 +0530

    this is the faq update

--- components/Faq.js
+++ components/Faq.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React from "react";
 import {
@@ -10,18 +10,22 @@ import {
 } from "react-accessible-accordion";
 import FaqData from "../Data/FaqData.json";
 
-export const Faq = () => {
+export const Faq = ({ Data }) => {
+  const faqItems = Data && Data.length > 0 ? Data : FaqData;
   return (
     <>
       <div className="faqcont col-lg-11 col-12">
         <Accordion allowZeroExpanded={true}>
-          {FaqData.map((item, index) => (
+          {faqItems.map((item, index) => (
             <AccordionItem key={index}>
               <AccordionItemHeading>
                 <AccordionItemButton>{item.question}</AccordionItemButton>
               </AccordionItemHeading>
               <AccordionItemPanel>
-                <div className="accordiancont">{item.answer}</div>
+                <div
+                  className="accordiancont"
+                  dangerouslySetInnerHTML={{ __html: item.answer }}
+                ></div>
               </AccordionItemPanel>
             </AccordionItem>
           ))}

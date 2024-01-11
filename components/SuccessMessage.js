d2851d904636782400608bdc48ff4fd520e15dc6
singhAkash790 <113873172+singhAkash790@users.noreply.github.com>
2024-01-11 11:27:53 +0530

    this is the loader upslide

--- components/SuccessMessage.js
+++ components/SuccessMessage.js
@@ -1,16 +1,31 @@
-"use client"
+"use client";
 import React from "react";
+import Sucess from "@/components/svg-components/Sucess";
+import { Line } from "./svg-components/Line";
+import { Dots } from "./svg-components/Dots";
 
 const SuccessMessage = ({ data }) => {
   // Access the collected data and display it in the success message
   const { productDetail, userData } = data;
 
   return (
-    <div>
-      <h2>Success!</h2>
-      <p>Product Detail: {JSON.stringify(productDetail)}</p>
-      <p>User Data: {JSON.stringify(userData)}</p>
-    </div>
+    <>
+      <section className="position-relative">
+        <div className="container">
+          <div className="web-container">
+            <div className="row">
+              <div className="col-md-12 col-12">
+                <div className="sucess_msg">
+                  <Sucess />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Dots className="hsection position-absolute svgwidth opacity-10" />
+        <Line className="svgwidthline position-absolute opacity-10 top-20 end-0" />
+      </section>
+    </>
   );
 };
 

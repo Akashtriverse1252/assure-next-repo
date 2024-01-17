c566a551ddd2f8f83f8605d94d8b2b7d1002eb22
Akashtriverse1252 <akash.triverse@gmail.com>
2024-01-17 15:05:14 +0530

    time slot code

--- components/SlotTime.js
+++ components/SlotTime.js
@@ -1,88 +1,52 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
-const SlotTime = () => {
-  const [currentTime, setCurrentTime] = useState(new Date());
-  const [selectedSlot, setSelectedSlot] = useState(null);
+const SlotTime = ({ timeSlots }) => {
+  const [selectedSlots, setSelectedSlots] = useState({});
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentTime(new Date());
-    }, 600000); // Update current time every minute
-
-    return () => clearInterval(interval);
-  }, []);
-
-  const formatTime = (hour, minute) => {
-    const period = hour >= 12 ? "PM" : "AM";
-    const formattedHour = hour % 12 === 0 ? 12 : hour % 12;
-    const formattedMinute = minute < 10 ? `0${minute}` : minute;
-    return `${formattedHour}:${formattedMinute} ${period}`;
+  const handleSlotSelect = (label, time) => {
+    const newSelectedSlots = { ...selectedSlots, [label]: time };
+    setSelectedSlots(newSelectedSlots);
   };
 
-  const handleSlotSelection = (formattedTime) => {
-    setSelectedSlot(formattedTime);
-  };
-
-  const generateTimeSlots = (startHour, endHour, interval) => {
-    const timeSlots = [];
-    const currentHour = currentTime.getHours();
-    const currentMinute = currentTime.getMinutes();
-    const currentPeriod = currentHour >= 12 ? "PM" : "AM";
-
-    for (let hour = startHour; hour <= endHour; hour += interval) {
-      const period = hour >= 12 ? "PM" : "AM";
-      const formattedTime = formatTime(Math.floor(hour), (hour % 1) * 60);
-
-      const isSelected =
-        selectedSlot === formattedTime &&
-        (hour > currentHour || (hour === currentHour && 30 >= currentMinute));
-
-      if (
-        (hour > currentHour && period === currentPeriod) ||
-        (hour > currentHour && period !== currentPeriod)
-      ) {
-        timeSlots.push(
-          <div key={formattedTime} className="app-border">
+  const renderTimeSlot = (label, times) => (
+    <div key={label}>
+      <p>{label}</p>
+      <div className="app-check">
+        {times.map((time) => (
+          <div
+            key={time}
+            className={`app-border  ${
+              selectedSlots[label] === time ? "selected" : ""
+            }`}
+          >
             <input
+              className="option-input"
+              id={time}
               type="radio"
-              className="option-input radio"
-              name={`slot-${formattedTime.replace(":", "").replace(" ", "-")}`}
-              id={`slot-${formattedTime.replace(":", "").replace(" ", "-")}`}
-              checked={isSelected}
-              onChange={() => handleSlotSelection(formattedTime)}
+              name="timeSlot"
+              onChange={() => handleSlotSelect(label, time)}
             />
             <label
-              className="app-label"
-              htmlFor={`slot-${formattedTime
-                .replace(":", "")
-                .replace(" ", "-")}`}
+              className={`app-label ${
+                selectedSlots[label] === time ? "selected" : ""
+              }`}
+              htmlFor={time}
             >
-              {formattedTime}
+              {time}
             </label>
           </div>
-        );
-      }
-    }
-    return timeSlots;
-  };
+        ))}
+      </div>
+    </div>
+  );
 
   return (
-    <>
+    <div className="Time-slot">
       <div className="app-time">
-        <div>
-          <p>Morning Slot</p>
-          <div className="app-check">{generateTimeSlots(7, 11.5, 0.5)}</div>
-        </div>
-        <div>
-          <p>Afternoon</p>
-          <div className="app-check">{generateTimeSlots(12, 15.5, 0.5)}</div>
-        </div>
-        <div>
-          <p>Evening</p>
-          <div className="app-check">{generateTimeSlots(16, 20, 0.5)}</div>
-        </div>
+        {timeSlots &&
+          timeSlots.map(({ label, times }) => renderTimeSlot(label, times))}
       </div>
-    </>
+    </div>
   );
 };
 

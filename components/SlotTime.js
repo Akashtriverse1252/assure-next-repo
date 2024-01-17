386bab62bf7165eeced57347ec450d6dbc665bd8
Akashtriverse1252 <akash.triverse@gmail.com>
2024-01-17 11:47:44 +0530

    time slot code

--- components/SlotTime.js
+++ components/SlotTime.js
@@ -2,19 +2,25 @@ import React, { useState, useEffect } from "react";
 
 const SlotTime = () => {
   const [currentTime, setCurrentTime] = useState(new Date());
+  const [selectedSlot, setSelectedSlot] = useState(null);
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentTime(new Date());
-    }, 600); // Update current time every minute
+    }, 600000); // Update current time every minute
 
     return () => clearInterval(interval);
   }, []);
 
-  const formatTime = (hour, minute, period) => {
+  const formatTime = (hour, minute) => {
+    const period = hour >= 12 ? "PM" : "AM";
     const formattedHour = hour % 12 === 0 ? 12 : hour % 12;
-    const formattedMinute = minute < 10 ? `0${minute}` : `${minute}`;
+    const formattedMinute = minute < 10 ? `0${minute}` : minute;
     return `${formattedHour}:${formattedMinute} ${period}`;
+  };
+
+  const handleSlotSelection = (formattedTime) => {
+    setSelectedSlot(formattedTime);
   };
 
   const generateTimeSlots = (startHour, endHour, interval) => {
@@ -25,22 +31,32 @@ const SlotTime = () => {
 
     for (let hour = startHour; hour <= endHour; hour += interval) {
       const period = hour >= 12 ? "PM" : "AM";
-      const formattedHour = hour % 12 === 0 ? 12 : hour % 12;
-      const formattedTime = formatTime(formattedHour, 0, period);
+      const formattedTime = formatTime(Math.floor(hour), (hour % 1) * 60);
+
+      const isSelected =
+        selectedSlot === formattedTime &&
+        (hour > currentHour || (hour === currentHour && 30 >= currentMinute));
 
       if (
         (hour > currentHour && period === currentPeriod) ||
-        (hour === currentHour && interval === 1 && 30 >= currentMinute)
+        (hour > currentHour && period !== currentPeriod)
       ) {
         timeSlots.push(
           <div key={formattedTime} className="app-border">
             <input
               type="radio"
               className="option-input radio"
-              name="slot1"
-              id={`slot-${formattedTime}`}
+              name={`slot-${formattedTime.replace(":", "").replace(" ", "-")}`}
+              id={`slot-${formattedTime.replace(":", "").replace(" ", "-")}`}
+              checked={isSelected}
+              onChange={() => handleSlotSelection(formattedTime)}
             />
-            <label className="app-label" htmlFor={`slot-${formattedTime}`}>
+            <label
+              className="app-label"
+              htmlFor={`slot-${formattedTime
+                .replace(":", "")
+                .replace(" ", "-")}`}
+            >
               {formattedTime}
             </label>
           </div>
@@ -55,11 +71,11 @@ const SlotTime = () => {
       <div className="app-time">
         <div>
           <p>Morning Slot</p>
-          <div className="app-check">{generateTimeSlots(7, 12, 0.5)}</div>
+          <div className="app-check">{generateTimeSlots(7, 11.5, 0.5)}</div>
         </div>
         <div>
           <p>Afternoon</p>
-          <div className="app-check">{generateTimeSlots(12, 16, 0.5)}</div>
+          <div className="app-check">{generateTimeSlots(12, 15.5, 0.5)}</div>
         </div>
         <div>
           <p>Evening</p>

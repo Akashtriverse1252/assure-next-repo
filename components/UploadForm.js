39d66b9ca5f6949e014613aa041b16a2c6ad2e03
Akashtriverse1252 <akash.triverse@gmail.com>
2024-02-06 11:05:04 +0530

    uploaing from start

--- components/UploadForm.js
+++ components/UploadForm.js
@@ -8,6 +8,8 @@ import Modal from "@mui/material/Modal";
 import axios from "axios";
 import { useData } from "@/context/context";
 import { useAlert } from "@/context/AlerterContext";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const API_URL =
   "http://assure.triverseadvertising.com/api/upload_prescription.php";
@@ -24,134 +26,49 @@ const allowedTypes = new Set([
 const UploadForm = () => {
   const { cartState, cartDispatch } = useData();
   const { showAlert } = useAlert();
-  const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const [fileState, setFileState] = useState({
-    fileName: "",
-    fileSize: "",
-    isFileUploaded: false,
-    filePreview: null,
-  });
-
-  const [uploadPrescription, setUploadPrescription] = useState();
-  const [formData, setFormData] = useState({
-    name: "",
-    phone: "",
-    email: "",
-    uploadPrescription: "",
-  });
-
-  const [errors, setErrors] = useState({
-    name: false,
-    phone: false,
-    email: false,
-    files: false,
-  });
-
-    const handleFileChange = ({ target: { files, name, value } }) => {
-      const selectedFile = files && files[0];
-      setUploadPrescription(selectedFile);
-
-      setFormData((prevValues) => ({
-        ...prevValues,
-        [name]: value,
-        uploadPrescription: selectedFile,
-      }));
-
-      if (selectedFile) {
-        handleValidFile(selectedFile);
-      } else {
-        handleInvalidFile();
-      }
-    };
-
-  const handleValidFile = (selectedFile) => {
-    if (
-      allowedTypes.has(selectedFile.type) &&
-      selectedFile.size <= 2 * 1024 * 1024
-    ) {
-      setFileState((prevState) => ({
-        ...prevState,
-        fileName: selectedFile.name,
-        fileSize: (selectedFile.size / 1024).toFixed(1) + " KB",
-        isFileUploaded: true,
-      }));
-
-      if (selectedFile.type.startsWith("image/")) {
-        handleImageFile(selectedFile);
-      }
-    } else {
-      handleInvalidFile(selectedFile);
-    }
-  };
-
-  const handleInvalidFile = (selectedFile) => {
-    setFileState({
-      fileName: "",
-      fileSize: "",
-      isFileUploaded: false,
-      filePreview: null,
-    });
-
-    let errorMessage = "Invalid files. ";
-
-    if (selectedFile && selectedFile.size > 2 * 1024 * 1024) {
-      errorMessage += "File size should be less than 2MB. ";
-    }
-
-    if (!selectedFile || !selectedFile.type || !selectedFile.size) {
-      errorMessage += "File is required. ";
-    }
-
-    if (
-      selectedFile &&
-      selectedFile.type &&
-      !allowedTypes.has(selectedFile.type)
-    ) {
-      errorMessage += "Invalid file type. ";
-    }
-
-    setErrors((prevErrors) => ({
-      ...prevErrors,
-      files: errorMessage,
-    }));
-
-    showAlert("File Error", errorMessage, "error");
-    console.error(errorMessage);
-  };
-
-  const handleImageFile = (selectedFile) => {
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      setFileState((prevState) => ({
-        ...prevState,
-        filePreview:
-          prevState.uploadPrescription === selectedFile.name
-            ? prevState.filePreview
-            : e.target.result,
-      }));
-    };
-    reader.readAsDataURL(selectedFile);
-  };
-
-  const handleRemoveFileClick = () => {
-    setFormData((prevValues) => ({
-      ...prevValues,
-      uploadPrescription: "",
-    }));
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (validateForm()) {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      phone: "",
+      email: "",
+      uploadPrescription: null,
+    },
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .required("Name is required")
+        .min(3, "Name should have a minimum length of 3 characters"),
+      phone: Yup.string()
+        .required("Phone Number is required")
+        .matches(
+          /^[0-9]{10}$/,
+          "Invalid Phone Number format (should be 10 digits)"
+        ),
+      email: Yup.string()
+        .required("Email is required")
+        .email("Invalid email format"),
+      uploadPrescription: Yup.mixed()
+        .required("File is required")
+        .test(
+          "fileType",
+          "Invalid file type",
+          (value) => value && allowedTypes.has(value.type)
+        )
+        .test(
+          "fileSize",
+          "File size should be less than 2MB",
+          (value) => value && value.size <= 2 * 1024 * 1024
+        ),
+    }),
+    onSubmit: async (values, { setSubmitting, resetForm }) => {
       try {
-        setIsSubmitting(true);
-        const formDataApi = {
-          name: formData.name,
-          email: formData.email,
-          phone: formData.phone,
-          uploadPrescription: uploadPrescription,
-        };
+        setSubmitting(true);
+
+        const formDataApi = new FormData();
+        formDataApi.append("name", values.name);
+        formDataApi.append("email", values.email);
+        formDataApi.append("phone", values.phone);
+        formDataApi.append("uploadPrescription", values.uploadPrescription);
 
         const response = await axios.post(API_URL, formDataApi, {
           headers: {
@@ -159,9 +76,7 @@ const UploadForm = () => {
           },
         });
 
-        // console.log("this is the api response ", response);
-
-        setIsSubmitting(false);
+        setSubmitting(false);
 
         const alertType =
           response.status === 200
@@ -176,64 +91,22 @@ const UploadForm = () => {
           alertType
         );
 
-        setFormData({
-          name: "",
-          phone: "",
-          email: "",
-          uploadPrescription: "",
-        });
+        resetForm();
       } catch (error) {
-        setIsSubmitting(false);
+        setSubmitting(false);
         showAlert("Error", "Network error", "error");
         console.error("Error:", error);
-        // Handle network errors here and display an error message to the user.
       }
-    } else {
-      showAlert("Error", "Fail to Submit form", "error");
-    }
+    },
+  });
+
+  const handleFileChange = (event) => {
+    formik.setFieldValue("uploadPrescription", event.currentTarget.files[0]);
+    formik.setFieldTouched("uploadPrescription", true);
   };
 
-  const validateForm = () => {
-    let isValid = true;
-    const newErrors = {};
-
-    const showError = (field, message) => {
-      setErrors((prevErrors) => ({
-        ...prevErrors,
-        [field]: message,
-      }));
-      showAlert("Error", message, "error");
-      newErrors[field] = true;
-      isValid = false;
-    };
-
-    if (formData.name.trim() === "") {
-      showError("name", "Name is required");
-    } else if (formData.name.trim().length < 3) {
-      showError("name", "Name should have a minimum length of 3 characters");
-    }
-
-    if (formData.phone.trim() === "") {
-      showError("phone", "Phone Number is required");
-    } else if (!/^[0-9]{10}$/.test(formData.phone)) {
-      showError("phone", "Invalid Phone Number format (should be 10 digits)");
-    }
-
-    if (formData.email.trim() === "") {
-      showError("email", "Email is required");
-    } else {
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      if (!emailRegex.test(formData.email)) {
-        showError("email", "Invalid email format");
-      }
-    }
-
-    if (!fileState.isFileUploaded) {
-      showError("files", "File is required");
-    }
-
-    setErrors(newErrors);
-    return isValid;
+  const handleRemoveFileClick = () => {
+    formik.setFieldValue("uploadPrescription", null);
   };
 
   return (
@@ -253,63 +126,70 @@ const UploadForm = () => {
             <div className=" prescription-form  flex-center flex-direction-row align-items-start">
               <div className=" float-start upload-prescription-form">
                 <h4>
-                  <strong>UPLOAD PRESCRIPTION</strong>{" "}
+                  <strong>UPLOAD PRESCRIPTION</strong>
                 </h4>
                 <div className="input-field flex-center flex-direction-column">
                   <TextField
                     type="text"
                     required
                     id="name"
-                    defaultValue="Name"
                     variant="standard"
                     label="Name"
                     name="name"
-                    value={formData.name}
-                    error={errors.name}
+                    value={formik.values.name}
+                    error={formik.touched.name && Boolean(formik.errors.name)}
+                    helperText={formik.touched.name && formik.errors.name}
                     fullWidth
-                    onChange={handleFileChange}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
                   />
                   <TextField
-                    label="Mobile Number"
-                    variant="standard"
-                    fullWidth
-                    type="text"
+                    type="tel"
                     required
-                    id="standard-required"
+                    id="phone"
+                    variant="standard"
+                    label="Contact Number"
                     name="phone"
-                    value={formData.phone}
-                    onChange={handleFileChange}
-                    error={errors.phone}
+                    value={formik.values.phone}
+                    error={formik.touched.phone && Boolean(formik.errors.phone)}
+                    helperText={formik.touched.phone && formik.errors.phone}
+                    fullWidth
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
                   />
                   <TextField
-                    label="Email"
-                    variant="standard"
-                    type="text"
-                    name="email"
+                    type="email"
                     required
-                    id="standard-required"
-                    value={formData.email}
-                    onChange={handleFileChange}
+                    id="email"
+                    variant="standard"
+                    label="Email"
+                    name="email"
+                    value={formik.values.email}
+                    error={formik.touched.email && Boolean(formik.errors.email)}
+                    helperText={formik.touched.email && formik.errors.email}
                     fullWidth
-                    error={errors.email}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
                   />
                 </div>
                 <button
                   type="button"
                   className="button button--aylen button--round-l button--text-thick mt-3"
-                  onClick={handleSubmit}
+                  onClick={formik.handleSubmit}
                 >
                   <span className="check-icon">
-                    {isSubmitting ? "Submitting..." : "SUBMIT"}
+                    {formik.isSubmitting ? "Submitting..." : "SUBMIT"}
                   </span>
                 </button>
               </div>
               <div className="drag-file-area ">
                 <div className="upload-icon">
-                  {fileState.isFileUploaded ? (
+                  {formik.values.uploadPrescription ? (
                     <div className="file-preview d-flex justify-content-center align-items-center">
                       <img
-                        src={fileState.filePreview}
+                        src={URL.createObjectURL(
+                          formik.values.uploadPrescription
+                        )}
                         alt="File Preview"
                         className="col-7"
                       />
@@ -319,7 +199,7 @@ const UploadForm = () => {
                   )}
                 </div>
                 <h3 className="dynamic-message">
-                  {fileState.isFileUploaded
+                  {formik.values.uploadPrescription
                     ? "File Successfully Selected!"
                     : "Select file here"}
                 </h3>
@@ -331,25 +211,34 @@ const UploadForm = () => {
                       accept="image/*"
                       type="file"
                       className="default-file-input"
-                      style={fileState.isFileUploaded ? { top: 0 } : {}}
+                      style={formik.values.uploadPrescription ? { top: 0 } : {}}
                       onChange={handleFileChange}
+                      onBlur={formik.handleBlur}
                     />
                     <span className="browse-files-text">
-                      {fileState.isFileUploaded
+                      {formik.values.uploadPrescription
                         ? "Browse again"
                         : "Browse file from device"}
                     </span>
-                    {fileState.isFileUploaded && <span>Change your file</span>}
+                    {formik.values.uploadPrescription && (
+                      <span>Change your file</span>
+                    )}
                   </div>
                 </label>
               </div>
             </div>
 
-            {fileState.isFileUploaded && (
+            {formik.values.uploadPrescription && (
               <div className="file-block">
                 <div className="file-info">
-                  <span className="file-name">{fileState.fileName}</span> |{" "}
-                  <span className="file-size">{fileState.fileSize}</span>
+                  <span className="file-name">
+                    {formik.values.uploadPrescription.name}
+                  </span>
+                  |
+                  <span className="file-size">
+                    {(formik.values.uploadPrescription.size / 1024).toFixed(1)}
+                    KB
+                  </span>
                 </div>
                 <div
                   className="material-icons remove-file-icon"

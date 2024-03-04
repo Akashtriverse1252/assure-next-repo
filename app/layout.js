ecac63b1c78314535bcfc5f601e70c29b9c8a7f7
Akashtriverse1252 <akash.triverse@gmail.com>
2024-03-04 17:12:24 +0530

    cganging the test to testpack in the oragan

--- app/layout.js
+++ app/layout.js
@@ -37,7 +37,7 @@ export default function RootLayout({ children, isLoading }) {
       <AlertProvider>
         <GlobalDataProvider>
           <body className={AvertaStd.className}>
-            {/* <Loader /> */}
+            <Loader />
             <div className="">
               <Header />
               <BreadCrums />
--- components/Loader.js
+++ components/Loader.js
@@ -1,37 +1,57 @@
-"use client";
+"use client"
 import { useEffect, useRef, useState } from "react";
 import LogoDNA from "@/components/svg-components/LogoDNA";
+import Cookies from "js-cookie";
 
 const Loader = () => {
   const mainLoaderRef = useRef(null);
+  const [isSaved, setIsSaved] = useState(true);
+
+  useEffect(() => {
+    const showLoader = Cookies.get("loaderHidden");
+
+    if (!showLoader) {
+      setIsSaved(false);
+      const loaderTimeout = setTimeout(() => {
+        Cookies.set("loaderHidden", true, { expires: 10 / (24 * 60) });
+        setIsSaved(true);
+      }, 3000);
+
+      return () => clearTimeout(loaderTimeout);
+    }
+  }, []);
 
   const hideLoaderSec = () => {
     document.querySelectorAll(".main_loader_sec").forEach((sec) => {
       sec.classList.add("mainSecLoaderLoaded");
     });
   };
+
   const hideLoader = () => {
     if (mainLoaderRef.current) {
       mainLoaderRef.current.classList.add("d-none");
     }
   };
 
-  // Automatically hide loader after 3 seconds
-  const timeout = setTimeout(hideLoaderSec, 4000);
-  const timeoutId = setTimeout(hideLoader, 4500);
-
-  // Cleanup the timeout to avoid memory leaks
   useEffect(() => {
-    return () => clearTimeout(timeoutId, timeout);
-  }, []);
+    const timeout = setTimeout(hideLoaderSec, 4000);
+    const timeoutId = setTimeout(hideLoader, 4500);
+
+    return () => {
+      clearTimeout(timeout);
+      clearTimeout(timeoutId);
+    };
+  }, [isSaved]);
 
   return (
     <>
+      {isSaved ? null : (
         <div className={`main_loader`} ref={mainLoaderRef}>
           <div className="main_loader_sec">
             <LogoDNA />
           </div>
         </div>
+      )}
     </>
   );
 };

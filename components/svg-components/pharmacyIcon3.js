6851f9017028ce07a4e30b9bafd05e4806201bac
Akashtriverse1252 <akash.triverse@gmail.com>
2024-05-17 17:12:13 +0530

    changes on pharmacy page

--- components/svg-components/pharmacyIcon3.js
+++ components/svg-components/pharmacyIcon3.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const pharmacyIcon3 = () => {
+const PharmacyIcon3 = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -23,4 +23,4 @@ const pharmacyIcon3 = () => {
   );
 };
 
-export default pharmacyIcon3;
+export default PharmacyIcon3;
